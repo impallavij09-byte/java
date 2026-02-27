82ba5cbaf7f505d9c92c3de97be17dbc68be8586
impallavij09-byte <impallavij09@gmail.com>
2026-02-27 13:00:38 +0000

    27/02

--- Ifelse.java
+++
@@ -1,3 +0,0 @@
-public class Ifelse{
-    public static void main (String  args
-}
---
+++ Overloading2.java
@@ -0,0 +1,11 @@
+public class Overloading2 {
+    public static void main(String[] args) {
+        System.out.println("hello world ");
+    }
+    public static void main(String args) {
+        System.out.println("hello world ");
+}
+public static void main(){
+        System.out.println("hello world ");
+}
+}
